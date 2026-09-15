44f3131031abca38974dba58669ea2bd4cb604e2
Bryan-t0 <brya.torres@duocuc.cl>
2026-09-15 18:18:34 -0300

    Corregir test backend para no consultar issuer externo

--- backend/src/test/java/cl/duoc/pedidos360/Pedidos360ApplicationTests.java
+++ backend/src/test/java/cl/duoc/pedidos360/Pedidos360ApplicationTests.java
@@ -2,8 +2,14 @@ package cl.duoc.pedidos360;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@SpringBootTest(properties = {"app.jwt.issuer=https://cognito-idp.us-east-1.amazonaws.com/us-east-1_example","app.jwt.client-id=test-client"})
+@SpringBootTest(properties = {"app.jwt.issuer=https://cognito-idp.us-east-1.amazonaws.com/us-east-1_ZzHU4WlkI","app.jwt.client-id=6t8gda2gsqh5h61jk5rgclc55s"})
 class Pedidos360ApplicationTests {
-  @Test void contextLoads() {}
+  @MockitoBean
+  JwtDecoder jwtDecoder;
+
+  @Test
+  void contextLoads() {}
 }
