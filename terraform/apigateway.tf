c9af0216c636c4eae455e62dd42955ab9b13a9cb
Bryan-t0 <brya.torres@duocuc.cl>
2026-09-15 17:34:57 -0300

    Corregir sintaxis Terraform en API Gateway

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -1,13 +1,44 @@
-resource "aws_apigatewayv2_api" "http" { name = "${var.project_name}-api" protocol_type = "HTTP" cors_configuration { allow_origins = ["*"] allow_methods = ["GET","OPTIONS"] allow_headers = ["authorization","content-type"] } }
+resource "aws_apigatewayv2_api" "http" {
+  name          = "${var.project_name}-api"
+  protocol_type = "HTTP"
 
-resource "aws_apigatewayv2_authorizer" "cognito" {
-  api_id = aws_apigatewayv2_api.http.id
-  authorizer_type = "JWT"
-  identity_sources = ["$request.header.Authorization"]
-  name = "cognito-jwt"
-  jwt_configuration { audience = [aws_cognito_user_pool_client.spa.id] issuer = "https://cognito-idp.${var.aws_region}.amazonaws.com/${aws_cognito_user_pool.pool.id}" }
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["GET", "OPTIONS"]
+    allow_headers = ["authorization", "content-type"]
+  }
 }
 
-resource "aws_apigatewayv2_integration" "datos" { api_id = aws_apigatewayv2_api.http.id integration_type = "HTTP_PROXY" integration_method = "GET" integration_uri = var.backend_integration_uri payload_format_version = "1.0" }
-resource "aws_apigatewayv2_route" "datos" { api_id = aws_apigatewayv2_api.http.id route_key = "GET /datos" target = "integrations/${aws_apigatewayv2_integration.datos.id}" authorization_type = "JWT" authorizer_id = aws_apigatewayv2_authorizer.cognito.id }
-resource "aws_apigatewayv2_stage" "default" { api_id = aws_apigatewayv2_api.http.id name = "$default" auto_deploy = true }
+resource "aws_apigatewayv2_authorizer" "cognito" {
+  api_id           = aws_apigatewayv2_api.http.id
+  authorizer_type  = "JWT"
+  identity_sources = ["$request.header.Authorization"]
+  name             = "cognito-jwt"
+
+  jwt_configuration {
+    audience = [aws_cognito_user_pool_client.spa.id]
+    issuer   = "https://cognito-idp.${var.aws_region}.amazonaws.com/${aws_cognito_user_pool.pool.id}"
+  }
+}
+
+resource "aws_apigatewayv2_integration" "datos" {
+  api_id                 = aws_apigatewayv2_api.http.id
+  integration_type       = "HTTP_PROXY"
+  integration_method     = "GET"
+  integration_uri        = var.backend_integration_uri
+  payload_format_version = "1.0"
+}
+
+resource "aws_apigatewayv2_route" "datos" {
+  api_id             = aws_apigatewayv2_api.http.id
+  route_key          = "GET /datos"
+  target             = "integrations/${aws_apigatewayv2_integration.datos.id}"
+  authorization_type = "JWT"
+  authorizer_id       = aws_apigatewayv2_authorizer.cognito.id
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http.id
+  name        = "$default"
+  auto_deploy = true
+}
